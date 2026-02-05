dbb1d6294544176a4afcd198934b2b65868740c0
Nikul <p.nikul6403@gmail.com>
2026-02-05 21:18:45 +0530

    Add Order's in views.py

--- Backend/serializers.py
+++ Backend/serializers.py
@@ -251,7 +251,19 @@ class OrderDetailSerializer(serializers.ModelSerializer):
                 'phone':addr.phone
             }
         return None
-        
+
+
+class OrderStatusUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['status']
+
+    def validate_status(self, value):
+        valid_statuses = ['pending', 'confirmed', 'shipped', 'delivered', 'cancelled']
+        if value not in valid_statuses:
+            raise serializers.ValidationError(f"Invalid status. Choose from: {valid_statuses}")
+        return value
+
     
 class CheckoutSerializer(serializers.Serializer):
     address_id = serializers.IntegerField()
--- Backend/views.py
+++ Backend/views.py
@@ -1,19 +1,22 @@
+from django.db.models.functions import Trunc
 from rest_framework.mixins import DestroyModelMixin
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
-from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet, ReadOnlyModelViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from .serializers import *
 from rest_framework import status
 from .models import *
 from rest_framework.generics import CreateAPIView, GenericAPIView, RetrieveAPIView, RetrieveUpdateAPIView, \
-    get_object_or_404, DestroyAPIView
+    get_object_or_404, DestroyAPIView, ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from django.db import transaction
-from rest_framework.decorators import action
+from rest_framework.decorators import action, permission_classes
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from decimal import Decimal
+from django.db.models import F
 
 
 class RegisterView(CreateAPIView):
@@ -245,3 +248,186 @@ class ProductViewSet(ModelViewSet):
         if self.action in ["list", "retrieve"]:
             return [AllowAny]
         return [IsAdminUser]
+    
+    
+TAX_RATE = Decimal('0.10')
+
+class CheckoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @transaction.atomic
+    def post(self, request):
+
+        serializer = CheckoutSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        address_id = serializer.validated_data["address_id"]
+        notes = serializer.validated_data.get("notes", "")
+
+        # Address validation
+        address = get_object_or_404(
+            Address,
+            id=address_id,
+            user=request.user
+        )
+
+        #  Lock cart
+        cart = (
+            Cart.objects
+            .select_for_update()
+            .prefetch_related("items__product")
+            .filter(user=request.user)
+            .first()
+        )
+
+        if not cart or not cart.items.exists():
+            return Response(
+                {"error": "Your cart is empty."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        cart_items = cart.items.all()
+
+        products = []
+
+        for item in cart_items:
+            product = item.product
+
+            if product.stock < item.quantity:
+                return Response(
+                    {
+                        "error": f'Not enough stock for "{product.name}". Only {product.stock} left.'
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            products.append(product)
+
+        #
+        subtotal = sum(
+            Decimal(item.item_total) for item in cart_items
+        )
+
+        TAX_RATE = Decimal("0.18")
+        tax = (subtotal * TAX_RATE).quantize(Decimal("0.01"))
+        total = subtotal + tax
+
+        # Create Order
+        order = Order.objects.create(
+            user=request.user,
+            shipping_address=address,
+            subtotal=subtotal,
+            tax=tax,
+            total=total,
+            notes=notes,
+            status="pending",
+            payment_status="pending",
+        )
+
+        #  create OrderItems
+        order_items = [
+            OrderItem(
+                order=order,
+                product=item.product,
+                product_name=item.product.name,
+                product_image=item.product.image,
+                unit_price=item.product.discounted_price,
+                quantity=item.quantity,
+            )
+            for item in cart_items
+        ]
+
+        OrderItem.objects.bulk_create(order_items)
+
+        #  Reduce stock efficiently
+        for product, item in zip(products, cart_items):
+            product.stock -= item.quantity
+        
+        # to improve performance and reduce db hits using bulk_update
+        Product.objects.bulk_update(products, ["stock"])
+
+        #  Clear cart
+        cart.items.all().delete()
+
+        return Response(
+            {
+                "message": "Order placed successfully 🎉",
+                "order": OrderDetailSerializer(order).data
+            },
+            status=status.HTTP_201_CREATED
+        )
+    
+class MyOrdersView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrderListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status']
+    
+    def get_queryset(self):
+        return Order.objects.filter(
+            user=self.request.user
+        ).select_related(
+            'shipping_address'
+        ).prefetch_related('item__product')
+    
+    
+class OrderViewSet(ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Order.objects.filter(
+            user=self.request.user
+        ).select_related(
+            'shipping_address'
+        ).prefetch_related('items__product')
+    
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return OrderDetailSerializer
+        return OrderListSerializer
+    
+    @action(detail=True, methods=['post'], url_path='cancel')
+    @transaction.atomic #to rollback everything if cancel turns wrong
+    def cancel_order(self, request, pk=None):
+        order = get_object_or_404(
+            Order.objects.select_for_update(),
+            id=pk,
+            user=request.user
+        )
+        
+        # Order check
+        if order.status in ('delivered', 'cancelled'):
+            return Response(
+                {'error': f'Cannot cancel with status "{order.status}"'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        # stock restore
+        for item in order.items.select_related('product'):
+            if item.product:
+                # F() ef db field ko directly db level par reference karta hai - without value Python me load liye
+                Product.objects.filter(pk=item.product_id).update(
+                    stock=F('stock') + item.quantity
+                )
+                
+        order.status = 'cancelled'
+        order.payment_status = 'refunded'
+        order.save(update_fields=['status', 'payment_status'])
+        
+        return Response(
+            {'status': 'Order cancelled', 'id':order.id},
+            status=status.HTTP_200_OK
+        )
+        
+        
+class OrderStatusUpdateView(RetrieveUpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderStatusUpdateSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
+    lookup_url_kwarg = 'order_id'
+
+    @transaction.atomic
+    def patch(self, request, *args, **kwargs):
+        # You can add extra logic here if needed
+        return super().patch(request, *args, **kwargs)
